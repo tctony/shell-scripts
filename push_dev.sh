b18aed50ffd3f4f846e30e127fd3b8758991e641
changtang(唐畅) <changtang@tencent.com>
2017-08-02 21:00:32 +0800

    changes from CHANGTANG-MC2 on 2017-08-02 21:00:32

---
+++ push_dev.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+git add .
+
+DEFAULT_COMMENT_MESSAGE='tmp commit for push dev'
+
+LAST_COMMIT_MESSAGE=`git log -1 --pretty=%B`
+
+if [ "$LAST_COMMIT_MESSAGE" = "$DEFAULT_COMMENT_MESSAGE" ]; then
+    git commit --amend -m "$DEFAULT_COMMENT_MESSAGE"
+else
+    git commit -m "$DEFAULT_COMMENT_MESSAGE"
+fi
+
+git push -f dev `git branch | grep \* | cut -d ' ' -f 2`:master
