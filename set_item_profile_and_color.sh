6b951958dfe00c7e104cc06e956ff2c5b150c5e3
changtang(唐畅) <changtang@tencent.com>
2017-08-02 20:56:12 +0800

    changes from CHANGTANG-MC2 on 2017-08-02 20:56:11

---
+++ set_item_profile_and_color.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# This script will change your iterm tab color and the background for that tab.
+
+# Requirements
+# - iterm as terminal
+# - a profile named Prod in iterm. You can configure it how ever you want. A profile can be found in Preferences -> Profiles
+
+# Set window profile ( in my case, Prod profile will make the background red)
+echo -e "\033]50;SetProfile=Server\a"
+
+# Set tab color to red
+echo -e "\033]6;1;bg;red;brightness;255\a"
