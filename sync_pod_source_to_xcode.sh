9a97d57f94a6291e5c9ff0d983bc88802a1577bd
changtang(唐畅) <changtang@tencent.com>
2017-08-02 20:54:01 +0800

    changes from CHANGTANG-MC2 on 2017-08-02 20:54:01

---
+++ sync_pod_source_to_xcode.sh
@@ -0,0 +1,32 @@
+# sync_pod_source_to_xcode.sh '/path/to/lib/source_dir' '/path/to/project/Pods/lib'
+
+if [ $# != 2 ]; then
+    echo "sync_pod_source_to_xcode.sh '/path/to/lib/source_dir' '/path/to/project/Pods/lib'"
+fi
+
+source_path=$1
+pod_path=$2
+
+cd $source_path && cd ..
+source_dir_name=`basename $source_path`
+for f in $(find $source_dir_name -name '*.[h,m]');
+do
+    fp=$pod_path/$f
+    if [ -e $fp ]; then
+        diff -q $fp $f
+        if [ $? != 0 ]; then
+            opendiff $fp $f -merge $fp
+        fi
+    else
+        read -r -p "\"$f\" was not found. Copy it? [y/N]" answer
+        case "$answer" in
+            [yY][eE][sS]|[yY]) 
+                cp $f $fp
+                ;;
+            *)
+                ;;
+        esac
+    fi
+done
+
+
