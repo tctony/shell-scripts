5032244d85c8b1481928e2927359e6302c728419
changtang(唐畅) <changtang@tencent.com>
2021-04-19 10:47:25 +0800

    support decode symbol of x64

--- decode_ios_crash_symbol.py
+++ decode_ios_crash_symbol.py
@@ -4,13 +4,12 @@ import sys
 import os
 
 def main():
-    dsym_path = os.popen("find . -name %s" % (sys.argv[1], )).read().strip()
-
-    os.system("xcrun atos -o %s -arch arm64 -l %s %s" % (dsym_path, sys.argv[2], sys.argv[3]))
+    dsym_path = os.popen("find . -name %s" % (sys.argv[2], )).read().strip()
+    os.system("xcrun atos -o %s -arch %s -l %s %s" % (dsym_path, sys.argv[1], sys.argv[3], sys.argv[4]))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("run '%s image_name base_addr symbol_addr' under dSYM directory" % sys.argv[0])
+    if len(sys.argv) < 5:
+        print("run '%s arch image_name base_addr symbol_addr' under dSYM directory" % sys.argv[0])
         exit(-1)
 
     main()
